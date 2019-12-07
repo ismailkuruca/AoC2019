db6ce036f0b92ac570b93a80e4ffd8bb8796b69d
ismail kuruca <ismailkuruca@gmail.com>
2019-12-07 07:40:04 +0100

    part2

--- src/main/java/com/ismailkuruca/aoc_2019/Day7.java
+++ src/main/java/com/ismailkuruca/aoc_2019/Day7.java
@@ -1,19 +1,22 @@
 package com.ismailkuruca.aoc_2019;
 
+import com.ismailkuruca.aoc_2019.util.FileUtil;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.ismailkuruca.aoc_2019.Day7.getNthDigit;
+
 public class Day7 {
 
-    private static int[] buffer = {0, 0, 0, 0, 0};
 
     public static void main(String[] args) {
         final List<String> day7 =
-//                FileUtil.readFile("day7");
-                Collections.singletonList("3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5");
+                FileUtil.readFile("day7");
+//                Collections.singletonList("3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5");
         //Collections.singletonList("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99");
 
         final List<String> day7Values = Arrays.asList(day7.get(0).split(","));
@@ -24,76 +27,95 @@ public class Day7 {
 
         final List<Integer> phases = Arrays.asList(9, 8, 7, 6, 5);
 
-        int output = 0;
-        try {
-            while (true) {
-                for (int i = 0; i < phases.size(); i++) {
-                    final Integer phase = phases.get(i);
-                    output = amp(input, phase, output);
-                    System.err.println(output);
+        int maxamp = 0;
+        for (int j = 0; j < 100000; j++) {
+            Collections.shuffle(phases);
+            final Machine machineA = new Machine();
+            machineA.phase = phases.get(0);
+            machineA.program = new ArrayList<>(input);
+            final Machine machineB = new Machine();
+            machineB.phase = phases.get(1);
+            machineB.program = new ArrayList<>(input);
+            final Machine machineC = new Machine();
+            machineC.phase = phases.get(2);
+            machineC.program = new ArrayList<>(input);
+            final Machine machineD = new Machine();
+            machineD.phase = phases.get(3);
+            machineD.program = new ArrayList<>(input);
+            final Machine machineE = new Machine();
+            machineE.phase = phases.get(4);
+            machineE.program = new ArrayList<>(input);
+
+            try {
+                while (true) {
+                    machineB.input = machineA.run();
+                    machineC.input = machineB.run();
+                    machineD.input = machineC.run();
+                    machineE.input = machineD.run();
+                    machineA.input = machineE.run();
+                }
+            } catch (Exception e) {
+                if (maxamp < machineE.output) {
+                    maxamp = machineE.output;
                 }
             }
-        } catch (Exception e) {
-            System.err.println(output);
         }
-//        int maxamp = 0;
-//        for (int j = 0; j < 100000; j++) {
-//            Collections.shuffle(phases);
-//            for (int i = 0; i < phases.size(); i++) {
-//                final int amp = amp(input, phases.get(0), amp(input, phases.get(1), amp(input, phases.get(2), amp(input, phases.get(3), amp(input, phases.get(4), 0)))));
-////                System.out.println(amp);
-//                if (amp > maxamp) {
-//                    maxamp = amp;
-//                }
-//            }
-//        }
-//
-//        System.out.println(maxamp);
-
-
+        System.err.println(maxamp);
     }
 
-    private static int amp(List<Integer> input, int phase, int signal) {
-        int pc = 0;
 
+    public static int getNthDigit(int number, int n) {
+        return (int) ((number / Math.pow(10, n - 1)) % 10);
+    }
+}
+
+class Machine {
+    public int input;
+    public int output;
+    List<Integer> program;
+    public int phase;
+    public int pc = 0;
+
+    public int run() {
         while (true) {
-            final Integer operation = input.get(pc);
+            final Integer operation = program.get(pc);
             final int opcode = operation % 100;
             if (opcode == 1) {
                 final int firstOperandMode = getNthDigit(operation, 3);
                 final int secondOperandMode = getNthDigit(operation, 4);
 
-                final Integer operand1 = firstOperandMode == 0 ? input.get(input.get(pc + 1)) : input.get(pc + 1);
-                final Integer operand2 = secondOperandMode == 0 ? input.get(input.get(pc + 2)) : input.get(pc + 2);
-                input.set(input.get(pc + 3), operand1 + operand2);
+                final Integer operand1 = firstOperandMode == 0 ? program.get(program.get(pc + 1)) : program.get(pc + 1);
+                final Integer operand2 = secondOperandMode == 0 ? program.get(program.get(pc + 2)) : program.get(pc + 2);
+                program.set(program.get(pc + 3), operand1 + operand2);
                 pc += 4;
             } else if (opcode == 2) {
                 final int firstOperandMode = getNthDigit(operation, 3);
                 final int secondOperandMode = getNthDigit(operation, 4);
 
-                final Integer operand1 = firstOperandMode == 0 ? input.get(input.get(pc + 1)) : input.get(pc + 1);
-                final Integer operand2 = secondOperandMode == 0 ? input.get(input.get(pc + 2)) : input.get(pc + 2);
-                input.set(input.get(pc + 3), operand1 * operand2);
+                final Integer operand1 = firstOperandMode == 0 ? program.get(program.get(pc + 1)) : program.get(pc + 1);
+                final Integer operand2 = secondOperandMode == 0 ? program.get(program.get(pc + 2)) : program.get(pc + 2);
+                program.set(program.get(pc + 3), operand1 * operand2);
                 pc += 4;
             } else if (opcode == 3) {
                 if (phase != -99) {
-                    input.set(input.get(pc + 1), phase);
+                    program.set(program.get(pc + 1), phase);
                     phase = -99;
                 } else {
-                    input.set(input.get(pc + 1), signal);
+                    program.set(program.get(pc + 1), input);
                 }
                 pc += 2;
             } else if (opcode == 4) {
                 final int firstOperandMode = getNthDigit(operation, 3);
 
-                final Integer operand1 = firstOperandMode == 0 ? input.get(input.get(pc + 1)) : input.get(pc + 1);
+                final Integer operand1 = firstOperandMode == 0 ? program.get(program.get(pc + 1)) : program.get(pc + 1);
                 pc += 2;
-                return operand1;
+                output = operand1;
+                return output;
             } else if (opcode == 5) {
                 final int firstOperandMode = getNthDigit(operation, 3);
                 final int secondOperandMode = getNthDigit(operation, 4);
-                final Integer operand1 = firstOperandMode == 0 ? input.get(input.get(pc + 1)) : input.get(pc + 1);
-                final Integer operand2 = secondOperandMode == 0 ? input.get(input.get(pc + 2)) : input.get(pc + 2);
+                final Integer operand1 = firstOperandMode == 0 ? program.get(program.get(pc + 1)) : program.get(pc + 1);
+                final Integer operand2 = secondOperandMode == 0 ? program.get(program.get(pc + 2)) : program.get(pc + 2);
 
                 if (!operand1.equals(0)) {
                     pc = operand2;
@@ -103,8 +125,8 @@ public class Day7 {
             } else if (opcode == 6) {
                 final int firstOperandMode = getNthDigit(operation, 3);
                 final int secondOperandMode = getNthDigit(operation, 4);
-                final Integer operand1 = firstOperandMode == 0 ? input.get(input.get(pc + 1)) : input.get(pc + 1);
-                final Integer operand2 = secondOperandMode == 0 ? input.get(input.get(pc + 2)) : input.get(pc + 2);
+                final Integer operand1 = firstOperandMode == 0 ? program.get(program.get(pc + 1)) : program.get(pc + 1);
+                final Integer operand2 = secondOperandMode == 0 ? program.get(program.get(pc + 2)) : program.get(pc + 2);
 
                 if (operand1.equals(0)) {
                     pc = operand2;
@@ -114,40 +136,22 @@ public class Day7 {
             } else if (opcode == 7) {
                 final int firstOperandMode = getNthDigit(operation, 3);
                 final int secondOperandMode = getNthDigit(operation, 4);
-                final Integer operand1 = firstOperandMode == 0 ? input.get(input.get(pc + 1)) : input.get(pc + 1);
-                final Integer operand2 = secondOperandMode == 0 ? input.get(input.get(pc + 2)) : input.get(pc + 2);
+                final Integer operand1 = firstOperandMode == 0 ? program.get(program.get(pc + 1)) : program.get(pc + 1);
+                final Integer operand2 = secondOperandMode == 0 ? program.get(program.get(pc + 2)) : program.get(pc + 2);
 
-                input.set(input.get(pc + 3), operand1 < operand2 ? 1 : 0);
+                program.set(program.get(pc + 3), operand1 < operand2 ? 1 : 0);
                 pc += 4;
             } else if (opcode == 8) {
                 final int firstOperandMode = getNthDigit(operation, 3);
                 final int secondOperandMode = getNthDigit(operation, 4);
-                final Integer operand1 = firstOperandMode == 0 ? input.get(input.get(pc + 1)) : input.get(pc + 1);
-                final Integer operand2 = secondOperandMode == 0 ? input.get(input.get(pc + 2)) : input.get(pc + 2);
+                final Integer operand1 = firstOperandMode == 0 ? program.get(program.get(pc + 1)) : program.get(pc + 1);
+                final Integer operand2 = secondOperandMode == 0 ? program.get(program.get(pc + 2)) : program.get(pc + 2);
 
-                input.set(input.get(pc + 3), operand1.equals(operand2) ? 1 : 0);
+                program.set(program.get(pc + 3), operand1.equals(operand2) ? 1 : 0);
                 pc += 4;
             } else {
                 throw new RuntimeException();
             }
         }
-    }
-
-
-    private static int getNthDigit(int number, int n) {
-        return (int) ((number / Math.pow(10, n - 1)) % 10);
-    }
-
-    static List<List<Integer>> permute(List<Integer> arr, int k) {
-        List<List<Integer>> output = new ArrayList();
-        for (int i = k; i < arr.size(); i++) {
-            java.util.Collections.swap(arr, i, k);
-            permute(arr, k + 1);
-            java.util.Collections.swap(arr, k, i);
-        }
-        if (k == arr.size() - 1) {
-            output.add(arr);
-        }
-        return output;
     }
 }
