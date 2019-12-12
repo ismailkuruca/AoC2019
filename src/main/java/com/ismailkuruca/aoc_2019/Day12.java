1e21cc89c2dc45e5f34004362b77977db7e020ce
ismail kuruca <ismailkuruca@gmail.com>
2019-12-12 07:30:47 +0100

    part2

--- src/main/java/com/ismailkuruca/aoc_2019/Day12.java
+++ src/main/java/com/ismailkuruca/aoc_2019/Day12.java
@@ -2,8 +2,11 @@ package com.ismailkuruca.aoc_2019;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
+import static org.apache.commons.math3.util.ArithmeticUtils.lcm;
 
 public class Day12 {
 
@@ -24,60 +27,60 @@ public class Day12 {
 //        final Moon moon3 = new Moon(4, -8, 8, 0, 0, 0);
 //        final Moon moon4 = new Moon(3, 5, -1, 0, 0, 0);
 
-//        final Moon moon1 = new Moon(-8 ,-10, 0, 0, 0, 0);
+//        final Moon moon1 = new Moon(-8, -10, 0, 0, 0, 0);
 //        final Moon moon2 = new Moon(5, 5, 10, 0, 0, 0);
 //        final Moon moon3 = new Moon(2, -7, 3, 0, 0, 0);
 //        final Moon moon4 = new Moon(9, -8, -3, 0, 0, 0);
 
-
         List<Moon> moons = asList(moon1, moon2, moon3, moon4);
-        List<Moon> temps = asList(moon1, moon2, moon3, moon4);
 
-        for (int i = 0; i < 1000; i++) {
+        final List<Integer> X = moons.stream()
+                .map(moon -> moon.x)
+                .collect(Collectors.toList());
+        final List<Integer> Y = moons.stream()
+                .map(moon -> moon.y)
+                .collect(Collectors.toList());
+        final List<Integer> Z = moons.stream()
+                .map(moon -> moon.z)
+                .collect(Collectors.toList());
 
-            moons.forEach(System.out::println);
-            for (int m = 0; m < moons.size(); m++) {
-                final Moon moonP = moons.get(m);
-                final Moon tempP = temps.get(m);
-                for (int n = m; n < moons.size(); n++) {
-                    if (m != n) {
-                        final Moon moonsS = moons.get(n);
-                        final Moon tempS = temps.get(n);
-                        if (moonP.x > moonsS.x) {
-                            tempP.vx -= 1;
-                            tempS.vx += 1;
-                        } else if (moonP.x < moonsS.x) {
-                            tempP.vx += 1;
-                            tempS.vx -= 1;
-                        }
-                        if (moonP.y > moonsS.y) {
-                            tempP.vy -= 1;
-                            tempS.vy += 1;
-                        } else if (moonP.y < moonsS.y) {
-                            tempP.vy += 1;
-                            tempS.vy -= 1;
-                        }
-                        if (moonP.z > moonsS.z) {
-                            tempP.vz -= 1;
-                            tempS.vz += 1;
-                        } else if (moonP.z < moonsS.z) {
-                            tempP.vz += 1;
-                            tempS.vz -= 1;
-                        }
-                    }
-                }
+        boolean perx = false, pery = false, perz = false;
+        long px = 1, py = 1, pz = 1;
 
+        long step = 0;
+        while (true) {
+            round(moons);
+
+            if (X.equals(moons.stream()
+                    .map(moon -> moon.x)
+                    .collect(Collectors.toList()))) {
+                perx = true;
             }
 
-            moons = new ArrayList<>(temps);
-
-            for (Moon moon : moons) {
-                moon.x += moon.vx;
-                moon.y += moon.vy;
-                moon.z += moon.vz;
+            if (Y.equals(moons.stream()
+                    .map(moon -> moon.y)
+                    .collect(Collectors.toList()))) {
+                pery = true;
             }
-            System.out.println("Step " + i);
+
+            if (Z.equals(moons.stream()
+                    .map(moon -> moon.z)
+                    .collect(Collectors.toList()))) {
+                perz = true;
+                System.out.println(Z);
+            }
+            px += perx ? 0 : 1;
+            py += pery ? 0 : 1;
+            pz += perz ? 0 : 1;
+
+            if (perx && pery && perz) {
+                System.err.println(px + " " + py + " " + pz) ;
+                System.out.println(lcm(px + 1, lcm(py + 1, pz + 1)));
+                break;
+            }
         }
+
+        System.out.println(step);
 
         int en = 0;
         for (Moon moon : moons) {
@@ -91,6 +94,49 @@ public class Day12 {
     }
 
 
+    private static List<Moon> round(List<Moon> moons) {
+        final List<Moon> temps = new ArrayList<>(moons);
+        for (int m = 0; m < moons.size(); m++) {
+            final Moon moonP = moons.get(m);
+            final Moon tempP = temps.get(m);
+            for (int n = m; n < moons.size(); n++) {
+                if (m != n) {
+                    final Moon moonsS = moons.get(n);
+                    final Moon tempS = temps.get(n);
+                    if (moonP.x > moonsS.x) {
+                        tempP.vx -= 1;
+                        tempS.vx += 1;
+                    } else if (moonP.x < moonsS.x) {
+                        tempP.vx += 1;
+                        tempS.vx -= 1;
+                    }
+                    if (moonP.y > moonsS.y) {
+                        tempP.vy -= 1;
+                        tempS.vy += 1;
+                    } else if (moonP.y < moonsS.y) {
+                        tempP.vy += 1;
+                        tempS.vy -= 1;
+                    }
+                    if (moonP.z > moonsS.z) {
+                        tempP.vz -= 1;
+                        tempS.vz += 1;
+                    } else if (moonP.z < moonsS.z) {
+                        tempP.vz += 1;
+                        tempS.vz -= 1;
+                    }
+                }
+            }
+
+        }
+
+        for (Moon moon : temps) {
+            moon.x += moon.vx;
+            moon.y += moon.vy;
+            moon.z += moon.vz;
+        }
+
+        return temps;
+    }
 }
 
 
@@ -107,12 +153,39 @@ class Moon {
                 '}' ;
     }
 
+    public Moon(Moon m) {
+        this.x = m.x;
+        this.y = m.y;
+        this.z = m.z;
+        this.vx = m.vx;
+        this.vz = m.vz;
+        this.vy = m.vy;
+    }
+
     int x;
     int y;
     int z;
     int vx;
     int vy;
     int vz;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Moon moon = (Moon) o;
+        return x == moon.x &&
+                y == moon.y &&
+                z == moon.z &&
+                vx == moon.vx &&
+                vy == moon.vy &&
+                vz == moon.vz;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z, vx, vy, vz);
+    }
 
     public Moon(int x, int y, int z, int vx, int vy, int vz) {
         this.x = x;
@@ -121,5 +194,39 @@ class Moon {
         this.vx = vx;
         this.vy = vy;
         this.vz = vz;
+
+
+    }
+
+
+}
+
+class Hash {
+    int i1;
+    int i2;
+    int i3;
+    int i4;
+
+    public Hash(List<Moon> moons) {
+        i1 = moons.get(0).hashCode();
+        i2 = moons.get(1).hashCode();
+        i3 = moons.get(2).hashCode();
+        i4 = moons.get(3).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hash hash = (Hash) o;
+        return i1 == hash.i1 &&
+                i2 == hash.i2 &&
+                i3 == hash.i3 &&
+                i4 == hash.i4;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(i1, i2, i3, i4);
     }
 }
