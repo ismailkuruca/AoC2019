f37eb35f4ec43d83ddc3001aea16e37567248eaa
ismail kuruca <ismailkuruca@gmail.com>
2019-12-19 06:02:16 +0100

    day18

--- src/main/java/com/ismailkuruca/aoc_2019/Day18.java
+++ src/main/java/com/ismailkuruca/aoc_2019/Day18.java
@@ -2,6 +2,7 @@ package com.ismailkuruca.aoc_2019;
 
 import com.ismailkuruca.aoc_2019.util.FileUtil;
 import org.jgrapht.GraphPath;
+import org.jgrapht.alg.connectivity.ConnectivityInspector;
 import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultDirectedWeightedGraph;
@@ -13,38 +14,73 @@ import java.util.stream.Collectors;
 
 public class Day18 {
     static List<String> knownKeys;
-
-    static List<String> day18;
+    static HashSet<String> globalKeys = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
 
-        day18 = FileUtil.readFile("day18");
+        List<String> day18_1 = FileUtil.readFile("day18_21");
+        List<String> day18_2 = FileUtil.readFile("day18_22");
+        List<String> day18_3 = FileUtil.readFile("day18_23");
+        List<String> day18_4 = FileUtil.readFile("day18_24");
+
         knownKeys = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
                 "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
                 "v", "w", "x", "y", "z", "q"));
 
 
-        loop(graph(day18), new HashSet<>(), "40,40,@", 0.0, 0);
+        final DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph1 = graph(day18_1, "39,39,@");
+        final DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph2 = graph(day18_2, "39,1,@");
+        final DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph3 = graph(day18_3, "1,39,@");
+        final DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph4 = graph(day18_4, "1,1,@");
+
+        ConnectivityInspector<String, DefaultWeightedEdge> connectivityInspector = new ConnectivityInspector<>(graph1);
+        final Set<String> graph1Keys = connectivityInspector.connectedSets().get(0).stream()
+                .filter(s -> s.matches(".*[a-z]"))
+                .map(s -> s.charAt(s.length() - 1) + "")
+                .collect(Collectors.toSet());
+        connectivityInspector = new ConnectivityInspector<>(graph2);
+        final Set<String> graph2Keys = connectivityInspector.connectedSets().get(0).stream()
+                .filter(s -> s.matches(".*[a-z]"))
+                .map(s -> s.charAt(s.length() - 1) + "")
+                .collect(Collectors.toSet());
+        connectivityInspector = new ConnectivityInspector<>(graph3);
+        final Set<String> graph3Keys = connectivityInspector.connectedSets().get(0).stream()
+                .filter(s -> s.matches(".*[a-z]"))
+                .map(s -> s.charAt(s.length() - 1) + "")
+                .collect(Collectors.toSet());
+        connectivityInspector = new ConnectivityInspector<>(graph4);
+        final Set<String> graph4Keys = connectivityInspector.connectedSets().get(0).stream()
+                .filter(s -> s.matches(".*[a-z]"))
+                .map(s -> s.charAt(s.length() - 1) + "")
+                .collect(Collectors.toSet());
+
+
+        final double loop3 = loop(graph3, new HashSet<>(), "1,39,@", 0.0, 0, graph3Keys);
+        final double loop2 = loop(graph2, new HashSet<>(), "39,1,@", 0.0, 0, graph2Keys);
+        final double loop4 = loop(graph4, new HashSet<>(), "1,1,@", 0.0, 0, graph4Keys);
+        final double loop1 = loop(graph1, new HashSet<>(), "39,39,@", 0.0, 0, graph1Keys);
+
+        System.err.println(loop1 + loop2 + loop3 + loop4);
 
     }
 
     static double min = Integer.MAX_VALUE;
     static int count = 0;
 
-    static void loop(DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph, Set<String> inventory, String start, double weight, int depth) {
+    static double loop(DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph, Set<String> inventory, String start, double weight, int depth, Set<String> knownKeys) {
         count++;
         if (count % 1000000 == 0) {
             System.err.println(count);
         }
         if (weight > min) {
-            return;
+            return weight;
         }
-        if (inventory.size() == 26) {
+        if (inventory.size() == knownKeys.size()) {
             if (weight < min) {
                 min = weight;
             }
             System.out.println(weight);
-            return;
+            return weight;
         }
         final Set<String> vertices = graph.vertexSet();
         DijkstraShortestPath<String, DefaultWeightedEdge> di = new DijkstraShortestPath<>(graph);
@@ -60,7 +96,7 @@ public class Day18 {
                 for (String ver : path.getVertexList()) {
                     final char c = ver.charAt(ver.length() - 1);
                     if (Character.isUpperCase(c)) {
-                        if (!inventory.contains(Character.toLowerCase(c) + "")) {
+                        if (!globalKeys.contains(Character.toLowerCase(c) + "")) {
                             broken = true;
                             break;
                         }
@@ -75,17 +111,20 @@ public class Day18 {
                 }
                 weight += path.getWeight();
                 if (weight > min) {
-                    return;
+                    return weight;
                 }
                 final Set<String> newInventory = new HashSet<>(inventory);
                 newInventory.add(knownKey);
-//                System.out.println(depth + " " + String.join("", newInventory) + " now: " + start + " next: " + vertex + " " + weight);
-                if (depth == 26 || newInventory.size() == inventory.size()) return;
-                loop(graph, newInventory, vertex, weight, depth + 1);
+                if (depth == knownKeys.size()) {
+                    globalKeys.addAll(knownKeys);
+                    return weight;
+                }
+                loop(graph, newInventory, vertex, weight, depth + 1, knownKeys);
             }
         }
-        if (inventory.size() == 26)
-            System.out.println(depth + " " + inventory + " " + weight);
+        if (inventory.size() == knownKeys.size())
+        globalKeys.addAll(inventory);
+        return weight;
     }
 
     static Map<String, ShortestPathAlgorithm.SingleSourcePaths<String, DefaultWeightedEdge>> CACHE = new HashMap<>();
@@ -98,7 +137,7 @@ public class Day18 {
         return CACHE.get(start).getPath(vertex);
     }
 
-    private static DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph(List<String> day18) {
+    private static DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> graph(List<String> day18, String start) {
         DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> directedGraph
                 = new DefaultDirectedWeightedGraph<>(DefaultWeightedEdge.class);
 
@@ -114,37 +153,25 @@ public class Day18 {
                         if (!directedGraph.containsVertex(i + "," + (j - 1) + "," + chars[j - 1])) {
                             directedGraph.addVertex(i + "," + (j - 1) + "," + chars[j - 1]);
                         }
-                        final DefaultWeightedEdge defaultWeightedEdge = directedGraph.addEdge(i + "," + j + "," + chars[j], i + "," + (j - 1) + "," + chars[j - 1]);
-//                        final DefaultWeightedEdge defaultWeightedEdge1 = directedGraph.addEdge(i + "," + (j - 1) + "," + chars[j - 1], i + "," + j + "," + chars[j]);
-//                        directedGraph.setEdgeWeight(defaultWeightedEdge, 1);
-//                        directedGraph.setEdgeWeight(defaultWeightedEdge1, 1);
+                        directedGraph.addEdge(i + "," + j + "," + chars[j], i + "," + (j - 1) + "," + chars[j - 1]);
                     }
                     if (j < chars.length - 1 && chars[j + 1] != '#') {
                         if (!directedGraph.containsVertex(i + "," + (j + 1) + "," + chars[j + 1])) {
                             directedGraph.addVertex(i + "," + (j + 1) + "," + chars[j + 1]);
                         }
-                        final DefaultWeightedEdge defaultWeightedEdge = directedGraph.addEdge(i + "," + j + "," + chars[j], i + "," + (j + 1) + "," + chars[j + 1]);
-//                        final DefaultWeightedEdge defaultWeightedEdge1 = directedGraph.addEdge(i + "," + (j + 1) + "," + chars[j + 1], i + "," + j + "," + chars[j]);
-//                        directedGraph.setEdgeWeight(defaultWeightedEdge, 1);
-//                        directedGraph.setEdgeWeight(defaultWeightedEdge1, 1);
+                        directedGraph.addEdge(i + "," + j + "," + chars[j], i + "," + (j + 1) + "," + chars[j + 1]);
                     }
                     if (i > 0 && day18.get(i - 1).toCharArray()[j] != '#') {
                         if (!directedGraph.containsVertex((i - 1) + "," + j + "," + day18.get(i - 1).toCharArray()[j])) {
                             directedGraph.addVertex((i - 1) + "," + j + "," + day18.get(i - 1).toCharArray()[j]);
                         }
-                        final DefaultWeightedEdge defaultWeightedEdge = directedGraph.addEdge(i + "," + j + "," + chars[j], (i - 1) + "," + j + "," + day18.get(i - 1).toCharArray()[j]);
-//                        final DefaultWeightedEdge defaultWeightedEdge1 = directedGraph.addEdge((i - 1) + "," + j + "," + day18.get(i - 1).toCharArray()[j], i + "," + j + "," + chars[j]);
-//                        directedGraph.setEdgeWeight(defaultWeightedEdge, 1);
-//                        directedGraph.setEdgeWeight(defaultWeightedEdge1, 1);
+                        directedGraph.addEdge(i + "," + j + "," + chars[j], (i - 1) + "," + j + "," + day18.get(i - 1).toCharArray()[j]);
                     }
                     if (i < day18.size() - 1 && day18.get(i + 1).toCharArray()[j] != '#') {
                         if (!directedGraph.containsVertex((i + 1) + "," + j + "," + day18.get(i + 1).toCharArray()[j])) {
                             directedGraph.addVertex((i + 1) + "," + j + "," + day18.get(i + 1).toCharArray()[j]);
                         }
-                        final DefaultWeightedEdge defaultWeightedEdge = directedGraph.addEdge(i + "," + j + "," + chars[j], (i + 1) + "," + j + "," + day18.get(i + 1).toCharArray()[j]);
-//                        final DefaultWeightedEdge defaultWeightedEdge1 = directedGraph.addEdge((i + 1) + "," + j + "," + day18.get(i + 1).toCharArray()[j], i + "," + j + "," + chars[j]);
-//                        directedGraph.setEdgeWeight(defaultWeightedEdge, 1);
-//                        directedGraph.setEdgeWeight(defaultWeightedEdge1, 1);
+                        directedGraph.addEdge(i + "," + j + "," + chars[j], (i + 1) + "," + j + "," + day18.get(i + 1).toCharArray()[j]);
                     }
                 }
             }
@@ -155,15 +182,15 @@ public class Day18 {
         final List<String> keys = knownKeys.stream()
                 .map(s -> s.charAt(0))
                 .map(s -> findVertex(vertices, s))
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
 
         final List<String> doors = knownKeys.stream()
                 .map(s -> s.charAt(0))
                 .map(Character::toUpperCase)
                 .map(s -> findVertex(vertices, s))
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
-
-        String start = "40,40,@";
 
         final DijkstraShortestPath<String, DefaultWeightedEdge> di = new DijkstraShortestPath<>(directedGraph);
 
@@ -172,33 +199,40 @@ public class Day18 {
 
         for (String door : doors) {
             if (start.equals(door)) continue;
-            final GraphPath<String, DefaultWeightedEdge> path = di.getPath(start, door);
-            final long length = path.getVertexList().stream()
-                    .filter(s -> !s.endsWith("."))
-                    .count();
-            if (length == 2) {
-                final double weight = path.getWeight();
-                final DefaultWeightedEdge defaultWeightedEdge = directedGraph.addEdge(start, door);
-                directedGraph.setEdgeWeight(defaultWeightedEdge, weight);
-            }
+            try {
+                final GraphPath<String, DefaultWeightedEdge> path = di.getPath(start, door);
+                if (path != null) {
+                    final long length = path.getVertexList().stream()
+                            .filter(s -> !s.endsWith("."))
+                            .count();
+                    if (length == 2) {
+                        final double weight = path.getWeight();
+                        final DefaultWeightedEdge defaultWeightedEdge = directedGraph.addEdge(start, door);
+                        directedGraph.setEdgeWeight(defaultWeightedEdge, weight);
+                    }
+                }
+            } catch (Exception e) {}
         }
 
         for (String s : doors) {
             for (String door : doors) {
                 if (s.equals(door)) continue;
-                final GraphPath<String, DefaultWeightedEdge> path = di.getPath(s, door);
-                final long length = path.getVertexList().stream()
-                        .filter(st -> !st.endsWith("."))
-                        .count();
-                if (length == 2) {
-                    final double weight = path.getWeight();
-                    final DefaultWeightedEdge defaultWeightedEdge = directedGraph.addEdge(s, door);
-                    if (defaultWeightedEdge != null) {
-                        directedGraph.setEdgeWeight(defaultWeightedEdge, weight);
+                try {
+                    final GraphPath<String, DefaultWeightedEdge> path = di.getPath(s, door);
+                    if (path != null) {
+
+                        final long length = path.getVertexList().stream()
+                                .filter(st -> !st.endsWith("."))
+                                .count();
+                        if (length == 2) {
+                            final double weight = path.getWeight();
+                            final DefaultWeightedEdge defaultWeightedEdge = directedGraph.addEdge(s, door);
+                            if (defaultWeightedEdge != null) {
+                                directedGraph.setEdgeWeight(defaultWeightedEdge, weight);
+                            }
+                        }
                     }
-                }
-//                final DefaultWeightedEdge defaultWeightedEdge1 = directedGraph.addEdge(s, door);
-//                directedGraph.setEdgeWeight(defaultWeightedEdge1, di.getPath(s, door).getWeight());
+                } catch (Exception e) {}
             }
         }
 
